c4a9bab849eb3f453651879c9553e6642e041db8
Canadew <110163919+chun-awa@users.noreply.github.com>
2024-07-26 19:27:51 +0800

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Used for Github Codespaces
+set -e
+
+echo "Installing ohmyzsh"
+sudo apt install zsh
+curl -L https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash - --unattended
+sudo chsh -s /usr/bin/zsh $USERNAME
+cp .zshrc ~
+
+echo "Installing plugins & powerlevel10k"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+cp .p10k.zsh ~
+
+echo "Installing tmux configuration"
+git clone --depth=1 https://github.com/gpakosz/.tmux ~/.tmux
+ln -sf ~/.tmux/.tmux.conf ~/.tmux.conf
+cp .tmux.conf.local ~
