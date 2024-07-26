69f0bdee8d98fe38333b8bbe817f18ce4c1653b4
Canadew <110163919+chun-awa@users.noreply.github.com>
2024-07-26 20:02:07 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -e
 [ $UID -eq 0 ] && exec sudo -u codespace $0
 
 echo "Setting up zsh"
-sudo chsh -s /usr/bin/zsh $USERNAME
+sudo chsh -s /usr/bin/zsh $(whoami)
 cp .zshrc ~
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
