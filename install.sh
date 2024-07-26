d16f6f7dbc8329890519ffad0b50f5e5558d820c
Canadew <110163919+chun-awa@users.noreply.github.com>
2024-07-26 19:45:12 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,14 +2,9 @@
 # Used for Github Codespaces
 set -e
 
-echo "Installing ohmyzsh"
-git clone --depth=1 https://github.com/ohmyzsh/ohmyzsh /tmp/ohmyzsh
-/tmp/ohmyzsh/tools/install.sh --unattended
-rm -rf /tmp/ohmyzsh
+echo "Setting up zsh"
 sudo chsh -s /usr/bin/zsh $USERNAME
 cp .zshrc ~
-
-echo "Installing plugins & powerlevel10k"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
