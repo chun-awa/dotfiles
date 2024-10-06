406b3087ebd4b41d1283b76cf0301296a09e2340
chun-awa <110163919+chun-awa@users.noreply.github.com>
2024-10-06 20:43:08 +0800

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+echo "Setting up zsh"
+git clone --depth=1 https://mirrors.tuna.tsinghua.edu.cn/git/ohmyzsh
+REMOTE=https://mirrors.tuna.tsinghua.edu.cn/git/ohmyzsh ohmyzsh/tools/install.sh --unattended
+rm -rf ohmyzsh
+sudo chsh -s /usr/bin/zsh $(whoami)
+cp .zshrc ~
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+cp .p10k.zsh ~
+
+echo "Setting up tmux"
+git clone --depth=1 https://github.com/gpakosz/.tmux ~/.tmux
+ln -sf ~/.tmux/.tmux.conf ~/.tmux.conf
+cp .tmux.conf.local ~
